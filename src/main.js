0ab16d2cfbe13b7f642e7956e59eb95986f4b5b4
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2024-07-20 19:00:12 +0800

    Removed main.js file outside the js folder

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import "./styles/components/footer.css";
+import "./styles/components/main.css";
+import "./styles/components/header.css";
+import "./styles/reset.css";
+import "./styles/style.css";
