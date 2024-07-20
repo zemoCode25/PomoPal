45bedf9724c5a9fb5f9218940d824521aca2c738
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2024-07-20 19:27:51 +0800

    Added the header section with settings button
    - Created the header section based on the mobile-first approach
    - Downloaded font styles and added the app logo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
+import "./styles/reset.css";
+import "./styles/style.css";
 import "./styles/components/footer.css";
 import "./styles/components/main.css";
 import "./styles/components/header.css";
-import "./styles/reset.css";
-import "./styles/style.css";
