4adc5c1523446890cf6021e56db3f7687696f616
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2024-07-21 19:56:39 +0800

    Added changing text content in main timer button
    - running and stopped data state for the button
    - Changing text content into Start and Pause.

--- src/js/time_progress.js
+++ src/js/time_progress.js
@@ -1,3 +1,5 @@
+const timerButton = document.querySelector("#timer__start");
+
 // document.addEventListener("DOMContentLoaded", function () {
 //   const display = document.querySelector(".timer__display");
 //   const path = document.querySelector(".timer__path");
@@ -18,3 +20,38 @@
 //     }
 //   }, 1000);
 // });
+
+function changeTimerButtonText() {
+  if (timerButton.dataset.state === "stopped") {
+    timerButton.textContent = "Pause";
+    timerButton.dataset.state = "running";
+  } else {
+    timerButton.textContent = "Start";
+    timerButton.dataset.state = "stopped";
+  }
+}
+
+timerButton.addEventListener("click", () => {
+  changeTimerButtonText();
+  const display = document.querySelector(".timer__display");
+  const path = document.querySelector(".timer__path");
+  let time = 1 * 60; // 25 minutes in seconds
+  let interval = setInterval(function () {
+    if (time <= 0) {
+      clearInterval(interval);
+      timerButton.textContent = "Start";
+      path.style.strokeDasharray = `0, 100`;
+      display.textContent = "1:00";
+    } else {
+      time -= 1;
+      let minutes = Math.floor(time / 60);
+      let seconds = time % 60;
+      display.textContent = `${minutes}:${
+        seconds < 10 ? "0" + seconds : seconds
+      }`;
+
+      let progress = (1 - time / (1 * 60)) * 100;
+      path.style.strokeDasharray = `${progress}, 100`;
+    }
+  }, 1000);
+});
