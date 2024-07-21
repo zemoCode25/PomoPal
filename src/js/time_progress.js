4ac6003074cff9f7db90a88275aa1cc58a8663ab
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2024-07-21 20:02:14 +0800

    Set the default values of the timer object

--- src/js/time_progress.js
+++ src/js/time_progress.js
@@ -1,25 +1,12 @@
 const timerButton = document.querySelector("#timer__start");
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const display = document.querySelector(".timer__display");
-//   const path = document.querySelector(".timer__path");
-//   let time = 1 * 60; // 25 minutes in seconds
-//   let interval = setInterval(function () {
-//     if (time <= 0) {
-//       clearInterval(interval);
-//     } else {
-//       time -= 1;
-//       let minutes = Math.floor(time / 60);
-//       let seconds = time % 60;
-//       display.textContent = `${minutes}:${
-//         seconds < 10 ? "0" + seconds : seconds
-//       }`;
-
-//       let progress = (1 - time / (1 * 60)) * 100;
-//       path.style.strokeDasharray = `${progress}, 100`;
-//     }
-//   }, 1000);
-// });
+// Timer Default values object;
+const timer = {
+  pomodoro: 25,
+  shortBreak: 5,
+  longBreak: 15,
+  session: 4,
+};
 
 function changeTimerButtonText() {
   if (timerButton.dataset.state === "stopped") {
