72ceeead42c01dc81931bd2e78f4e108859525e1
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2024-07-22 10:55:09 +0800

    Added UI transition between timer options

--- src/js/time_progress.js
+++ src/js/time_progress.js
@@ -1,4 +1,4 @@
-const timerButton = document.querySelector("#timer__start");
+const timerStart = document.querySelector("#timer__start");
 
 // Timer Default values object;
 const timer = {
@@ -8,21 +8,37 @@ const timer = {
   session: 4,
 };
 
+const timerButtons = document.querySelectorAll(".timer__button");
+
+function removeActiveButton() {
+  timerButtons.forEach((button) => {
+    button.classList.remove("active__button");
+  });
+}
+
+timerButtons.forEach((timerButton) => {
+  timerButton.addEventListener("click", () => {
+    removeActiveButton();
+    timerButton.classList.add("active__button");
+  });
+});
+
 function changeTimerButtonText() {
-  if (timerButton.dataset.state === "stopped") {
-    timerButton.textContent = "Pause";
-    timerButton.dataset.state = "running";
+  if (timerStart.dataset.state === "stopped") {
+    timerStart.textContent = "Pause";
+    timerStart.dataset.state = "running";
   } else {
-    timerButton.textContent = "Start";
-    timerButton.dataset.state = "stopped";
+    timerStart.textContent = "Start";
+    timerStart.dataset.state = "stopped";
   }
 }
 
-timerButton.addEventListener("click", () => {
+timerStart.addEventListener("click", () => {
   changeTimerButtonText();
   const display = document.querySelector(".timer__display");
   const path = document.querySelector(".timer__path");
-  let time = 1 * 60; // 25 minutes in seconds
+  let time = timer.pomodoro * 60; // 25 minutes in seconds
+
   let interval = setInterval(function () {
     if (time <= 0) {
       clearInterval(interval);
@@ -30,14 +46,14 @@ timerButton.addEventListener("click", () => {
       path.style.strokeDasharray = `0, 100`;
       display.textContent = "1:00";
     } else {
-      time -= 1;
+      timer.pomodoro -= 1;
       let minutes = Math.floor(time / 60);
       let seconds = time % 60;
       display.textContent = `${minutes}:${
         seconds < 10 ? "0" + seconds : seconds
       }`;
 
-      let progress = (1 - time / (1 * 60)) * 100;
+      let progress = (1 - timer.pomodoro / (1 * 60)) * 100;
       path.style.strokeDasharray = `${progress}, 100`;
     }
   }, 1000);
