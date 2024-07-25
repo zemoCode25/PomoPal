430d5d045b2d9f1a7c51d6f18cc2fe3fff4663d7
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2024-07-25 12:51:35 +0800

    Changed the name of main.css to timer.css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "./styles/reset.css";
 import "./styles/style.css";
 import "./styles/components/footer.css";
-import "./styles/components/main.css";
+import "./styles/components/timer.css";
 import "./styles/components/header.css";
