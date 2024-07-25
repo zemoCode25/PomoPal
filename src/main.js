f442a97440e869cd358ae251d7e36e8b99d42e34
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2024-07-25 13:13:36 +0800

    Added initial form styling in todo section and utils.css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import "./styles/reset.css";
 import "./styles/style.css";
+import "./styles/utils.css";
 import "./styles/components/footer.css";
 import "./styles/components/timer.css";
+import "./styles/components/todo.css";
 import "./styles/components/header.css";
