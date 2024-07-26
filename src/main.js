3b014935039077d53484597678eff54577c48e8a
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2024-07-26 20:49:10 +0800

    Finished facts section

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import "./styles/style.css";
 import "./styles/utils.css";
 import "./styles/components/footer.css";
 import "./styles/components/timer.css";
+import "./styles/components/facts.css";
 import "./styles/components/todo.css";
 import "./styles/components/header.css";
