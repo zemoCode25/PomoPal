ab6bf05e8839e75200d3e3839d499efa877df030
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2024-07-31 15:15:21 +0800

    Finished UI of Pop up modal settings

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import "./styles/reset.css";
 import "./styles/style.css";
 import "./styles/utils.css";
+import "./styles/components/overlay.css";
 import "./styles/components/footer.css";
 import "./styles/components/timer.css";
 import "./styles/components/facts.css";
