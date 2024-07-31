8c7b8bdebc3cf46e4c033b87bb6274a611c07449
Ralph Bryan Carlos <ralphbryancarlos27@gmail.com>
2024-07-31 15:44:11 +0800

    Created modal.js file and transferred setting button eventlistener

---
+++ src/js/modal.js
@@ -0,0 +1,4 @@
+settingButton.addEventListener("click", () => {
+  overlay.classList.add("overlay-active");
+  setting.classList.add("setting-active");
+});
--- src/js/overlay.js
+++ src/js/overlay.js
@@ -3,11 +3,6 @@ const box = document.querySelector(".box");
 const settingButton = document.querySelector("#header_button");
 const setting = document.querySelector(".setting");
 
-settingButton.addEventListener("click", () => {
-  overlay.classList.add("overlay-active");
-  setting.classList.add("setting-active");
-});
-
 overlay.addEventListener("click", () => {
   overlay.classList.remove("overlay-active");
   setting.classList.remove("setting-active");
